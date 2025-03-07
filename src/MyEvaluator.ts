4b848cf2366c3c04eb96cdb84244229e788c1d01
Jianwen <jianwenlei2003@gmail.com>
2025-03-07 20:46:56 +0800

    Use the parser in the evaluator

--- src/MyEvaluator.ts
+++ src/MyEvaluator.ts
@@ -1,19 +1,51 @@
+import antlr4 from 'antlr4';
+import SimpleLangLexer from './antlr/SimpleLangLexer.js';
+import SimpleLangParser from './antlr/SimpleLangParser.js';
+import SimpleLangVisitor from './antlr/SimpleLangVisitor.js';
 import { BasicEvaluator } from "conductor/dist/conductor/runner";
 import { IRunnerPlugin } from "conductor/dist/conductor/runner/types";
 
-const eval2 = eval;
+// extend the generated visitor to evaluate expressions
+class EvalVisitor extends SimpleLangVisitor {
+    visitProg(ctx: any): number {
+        return this.visit(ctx.expression());
+    }
 
-export class MyEvaluator extends BasicEvaluator {
-    private someEvaluatorState: number;
+    visitExpression(ctx: any): number {
+        if (ctx.INT()) {
+            return parseInt(ctx.INT().getText(), 10);
+        } else if (ctx.getChildCount() === 3) {
+            const left = this.visit(ctx.getChild(0));
+            const op = ctx.getChild(1).getText();
+            const right = this.visit(ctx.getChild(2));
+            switch (op) {
+                case '+': return left + right;
+                case '-': return left - right;
+                case '*': return left * right;
+                case '/': return left / right;
+            }
+        }
+        return 0;
+    }
+}
 
+export class SimpleLangEvaluator extends BasicEvaluator {
     async evaluateChunk(chunk: string): Promise<void> {
-        this.someEvaluatorState++;
-        eval2(chunk);
-        this.conductor.sendOutput(`Chunk ${this.someEvaluatorState} has been evaluated!`);
+        // set up antlr's pipeline
+        const chars = new antlr4.InputStream(chunk);
+        const lexer = new SimpleLangLexer(chars);
+        const tokens = new antlr4.CommonTokenStream(lexer);
+        const parser = new SimpleLangParser(tokens);
+        parser.buildParseTrees = true;
+        const tree = parser.prog();
+
+        // evaluate using our visitor
+        const visitor = new EvalVisitor();
+        const result = visitor.visit(tree);
+        this.conductor.sendOutput(`result: ${result}`);
     }
 
     constructor(conductor: IRunnerPlugin) {
         super(conductor);
-        this.someEvaluatorState = 0;
     }
 }
